c0cbf108a1447dbc8cd6f142192130f1f6a1d9c9
Charlie <neycbs@outlook.com>
2022-05-14 19:08:39 +0800

    bug fix

--- main.js
+++ main.js
@@ -8,10 +8,19 @@ const app = express()
 const port = 80
 const dirname = 'note'
 
+function randStr(
+  len,
+  dict = '234567890abcdefghjkmnopqrstuvwxyzABCDEFGHJKMNOPQRSTUVWXYZ'
+) {
+  var result = []
+  while (len--) result.push(~~(Math.random() * dict.length))
+  return result.map((v) => dict[v]).join('')
+}
+
 app.use(cors())
-app.get(['/', '/${dir}/'], async (req, res) => {
-  let key = randomUUID().slice(0, 4)
-  while (await database.getData(key)) key = randomUUID().slice(0, 4)
+app.get(['/', `/${dirname}/`], async (req, res) => {
+  let key = randStr(4)
+  while (await database.getData(key)) key = randStr(4)
   res.redirect(`/${dirname}/${key}/`)
 })
 app.use(
@@ -23,7 +32,7 @@ app.use(
   express.static('public')
 )
 
-app.use(express.json({ limit: '10mb' }))
+app.use(express.json({ limit: '1mb' }))
 app.get(`/api/${dirname}/get`, api.getNote)
 app.put(`/api/${dirname}/edit`, api.editNote)
 app.listen(port, () => {
